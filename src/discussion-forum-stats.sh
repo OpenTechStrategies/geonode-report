2db34313b092a375a891ee6033503db1d2b2283b
Karl Fogel <kfogel@red-bean.com>
2016-05-05 21:13:56 -0500

    Adjust script for commit b874af14

--- src/discussion-forum-stats.sh
+++ src/discussion-forum-stats.sh
@@ -11,7 +11,7 @@ for name in geonode-users-stats geonode-dev-stats; do
   echo ""
   for year in 2010 2011 2012 2013 2014 2015; do
     for month in 01 02 03 04 05 06 07 08 09 10 11 12; do
-      grep -E " - [0-9]{1,2}/${month}/${year}" ${name}
+      grep -E " - ${year}/${month}/[0-9]{1,2}" ${name}
     done
   done
 done
