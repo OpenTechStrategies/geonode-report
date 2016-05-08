58938b3991a1ef7ae1a9c4f8f724899966cafd63
Karl Fogel <kfogel@red-bean.com>
2016-05-07 23:41:12 -0500

    Plot users forum views as well as posts

--- src/forum-activity.sh
+++ src/forum-activity.sh
@@ -3,6 +3,13 @@
 # Generate a bar chart about GeoNode users forum activity.
 # See forum-activity.el for how data gets generated.
 
-rm -f forum-activity-posts.png
-gnuplot forum-activity.plt
-gimp forum-activity-posts.png &
+rm -f forum-activity-posts.png forum-activity-views.png
+
+sed -e 's/___THING___/posts/g' \
+    < forum-activity.plt.in > forum-activity-posts.plt
+sed -e 's/___THING___/views/g' \
+    < forum-activity.plt.in > forum-activity-views.plt
+
+gnuplot forum-activity-posts.plt
+gnuplot forum-activity-views.plt
+gimp forum-activity-posts.png forum-activity-views.png &
