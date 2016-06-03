ec1f96ef5544cde57790cde69c5222b9b41a3b86
cecilia-donnelly <cdonnelly@opentechstrategies.com>
2016-06-03 17:30:32 -0500

    Update closed-in-one-day chart
    Include months with no issues filed and make labels more human-readable.

--- src/rate-issues-1-day.py
+++ src/rate-issues-1-day.py
@@ -11,22 +11,36 @@ import json
 from pprint import pprint
 from dateutil.parser import parse
 import datetime
+from datetime import date, timedelta
 import operator
 import matplotlib.pyplot as plt
 import numpy as np
 
-with open('issues-list-short.json') as data_file:    
+with open('issues-list-safe.json') as data_file:    
     issue_data = json.load(data_file)
 
 month_years = {}
 # loop over issue_data['issues'] and count issues per month
 for issue in issue_data['issues']:
+    # ignore pull requests
+    try:
+        issue['pull_request']
+        continue
+    except:
+        # if this isn't a pull request, carry on
+        None
+        
     created_date = parse(issue['created_at'])
     closed_date = None
     if issue['closed_at']:
         closed_date = parse(issue['closed_at'])
+
+    # make the months order correctly
+    if len(str(created_date.month)) < 2:
+        this_month_year = str(created_date.year) + '-0' + str(created_date.month)
+    else:
+        this_month_year = str(created_date.year) + '-' + str(created_date.month)
     
-    this_month_year = str(created_date.year) + '-' + str(created_date.month)
     # add month-year array element to an array of month-years, and
     # increment a counter in that element (to account for out-of-order
     # issues)
@@ -45,9 +59,14 @@ for issue in issue_data['issues']:
 months = []
 issue_count = []
 closed_count = []
-# TODO: fix sorting! (currently the order has 2010-10, 2010-11, 2010-2)...
-# TODO: account for months with no issues filed
+# account for months with no issues filed (sorry about this...)
+missing_months = ['2010-03', '2010-05', '2010-09', '2011-10', '2011-11', '2012-01', '2012-02', '2012-03']
+
+for month in missing_months:
+    month_years[month] = {'issue_counter': 0, 'closed_counter': 0}
+
 sorted_months = sorted(month_years.items(), key=operator.itemgetter(0))
+
 for month, counts in sorted_months:
     months.append(month)
     issue_count.append(counts['issue_counter'])
@@ -66,9 +85,9 @@ p1 = plt.bar(ind, issue_count, width, color='b')
 p2 = plt.bar(ind, closed_count, width, color='g',
              bottom=issue_count)
 
-plt.ylabel('Number of issues')
-plt.title('Issues, open less than and greater than one day')
-plt.xticks(np.arange(0, N, 12))
+plt.ylabel('Number of issues opened')
+plt.title('Issues: all and those closed within one day')
+plt.xticks(np.arange(0, N, 12), ('December 2009', 'December 2010','December 2011','December 2012','December 2013','December 2014','December 2015'))
 plt.yticks(np.arange(0, 170, 10))
 plt.legend((p1[0], p2[0]), ('Number of issues', 'Number of issues closed in a day or less'))
 
