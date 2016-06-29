c07943db38c711addd6eade93e71f9dcf16b8473
Karl Fogel <kfogel@red-bean.com>
2016-06-29 14:32:34 -0500

    Aggregate GeoNode mailing list information
    Aggregate information from the new (starting 2015-02) Mailman lists at
    osgeo.org (linked from http://geonode.org/communication/#mailing-lists).
    Also move our existing older Google Group data files to be nearby.

---
+++ data/mailing-lists/mailman/geonode-devel-2015-2016/download.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+ROOT=http://lists.osgeo.org/pipermail/geonode-devel/
+
+wget ${ROOT}/2016-June.txt.gz
+sleep 1
+
+wget ${ROOT}/2016-May.txt.gz
+sleep 1
+
+wget ${ROOT}/2016-April.txt.gz
+sleep 1
+
+wget ${ROOT}/2016-March.txt.gz
+sleep 1
+
+wget ${ROOT}/2016-February.txt.gz
+sleep 1
+
+wget ${ROOT}/2016-January.txt.gz
+sleep 1
+
+wget ${ROOT}/2015-December.txt.gz
+sleep 1
+
+wget ${ROOT}/2015-November.txt.gz
+sleep 1
+
+wget ${ROOT}/2015-October.txt.gz
+sleep 1
+
+wget ${ROOT}/2015-September.txt.gz
+sleep 1
+
+wget ${ROOT}/2015-August.txt.gz
+sleep 1
+
+wget ${ROOT}/2015-July.txt.gz
+sleep 1
+
+wget ${ROOT}/2015-June.txt.gz
+sleep 1
+
+wget ${ROOT}/2015-May.txt.gz
+sleep 1
+
+wget ${ROOT}/2015-April.txt.gz
+sleep 1
+
+wget ${ROOT}/2015-March.txt.gz
+sleep 1
+
+wget ${ROOT}/2015-February.txt.gz
+sleep 1
